2bc2cb000bbd04ee35ad8875035a7f013c96a5a8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 10:05:43 -0300

    first commit

---
+++ src/main/java/br/com/fiap/ovnis/OvnisApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ovnis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvnisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvnisApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/ovnis/register/RegisterOvni.java
@@ -0,0 +1,5 @@
+package br.com.fiap.ovnis.register;
+
+public class RegisterOvni {
+    
+}
