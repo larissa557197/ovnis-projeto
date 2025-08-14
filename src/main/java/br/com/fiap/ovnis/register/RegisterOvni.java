90e4b5c3c3c340080d8b945fd0eb61ee16957b79
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 10:39:18 -0300

    add atributos na classe RegisterOvni

--- src/main/java/br/com/fiap/ovnis/register/RegisterOvni.java
+++ src/main/java/br/com/fiap/ovnis/register/RegisterOvni.java
@@ -1,5 +1,29 @@
 package br.com.fiap.ovnis.register;
 
+import java.time.LocalDateTime;
+
+import br.com.fiap.ovnis.register.enums.ShapeEnum;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterOvni {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String location;
+    private LocalDateTime localDateTime;
+    private ShapeEnum shapeEnum;
+    private int quantity;
     
 }
