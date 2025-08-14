6e6f5e72dbc2967ff054512bafbc8df57072bb00
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 11:20:15 -0300

    ajustando o bd

--- src/main/java/br/com/fiap/ovnis/register/RegisterOvni.java
+++ src/main/java/br/com/fiap/ovnis/register/RegisterOvni.java
@@ -3,7 +3,10 @@ package br.com.fiap.ovnis.register;
 import java.time.LocalDateTime;
 
 import br.com.fiap.ovnis.register.enums.ShapeEnum;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,7 +25,12 @@ public class RegisterOvni {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String location;
+
+    @Column(name = "local_date_time")
     private LocalDateTime localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "shape_enum")
     private ShapeEnum shapeEnum;
     private int quantity;
     
--- src/main/java/br/com/fiap/ovnis/register/RegisterOvniRepository.java
+++ src/main/java/br/com/fiap/ovnis/register/RegisterOvniRepository.java
@@ -3,5 +3,5 @@ package br.com.fiap.ovnis.register;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegisterOvniRepository extends JpaRepository<RegisterOvni, Long> {
-    //
+    
 }
